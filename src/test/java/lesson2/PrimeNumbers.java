efc16897431feb0c9cbd8a9c12a1ab8ac77ab7ad
shyshak <shyshak.av@gmail.com>
2014-10-31 21:21:34 +0200

    primeNumber

---
+++ src/test/java/lesson2/PrimeNumbers.java
@@ -0,0 +1,35 @@
+package lesson2;
+
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created with IntelliJ IDEA.
+ * User: Nastya
+ * Date: 23.10.14
+ * Time: 14:53
+ * To change this template use File | Settings | File Templates.
+ */
+public class PrimeNumbers {
+      @Test
+      public void isPrimaryNum()
+      {
+          Assert.assertEquals(isPrime(12), false);
+          Assert.assertEquals(isPrime(3), true);
+      }
+
+
+
+    public static boolean isPrime(int num)
+    {
+        for(int k = 2; k<num; k++)
+        {
+            if(num%k == 0)
+                return false;
+        }
+        return true;
+    }
+    public static void main(String[] args) {
+        System.out.println(isPrime(121));
+    }
+}
